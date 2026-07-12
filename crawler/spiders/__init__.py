ded6e3f4e89648422ca539d3bd25a107e0b86dfc
tommyxue552 <615212517@qq.com>
2026-07-12 12:20:46 +0800

    修复

--- crawler/spiders/__init__.py
+++
@@ -1,8 +0,0 @@
-﻿# crawler/spiders/__init__.py
-# ===========================
-# 采集规则模块。
-# 每个资源站对应一个 spider 文件，继承 Crawler 基类并实现 parse 方法。
-
-from .gameshare import GameshareSpider
-
-__all__ = ["GameshareSpider"]
