b01fb2f6b18c20684dab50e20bd8eed76364cf3d
tommyxue552 <615212517@qq.com>
2026-07-12 10:37:18 +0800

    模块6

---
+++ crawler/__init__.py
@@ -0,0 +1,13 @@
+﻿# crawler/__init__.py
+# ===================
+# 独立采集服务包。
+
+from .base import Crawler, CrawlResult, FetchEngine, RequestsEngine, PlaywrightEngine
+
+__all__ = [
+    "Crawler",
+    "CrawlResult",
+    "FetchEngine",
+    "RequestsEngine",
+    "PlaywrightEngine",
+]
---
+++ crawler/spiders/__init__.py
@@ -0,0 +1,4 @@
+﻿# crawler/spiders/__init__.py
+# ===========================
+# 采集规则模块。
+# 每个资源站对应一个 spider 文件，继承 Crawler 基类并实现 parse 方法。
